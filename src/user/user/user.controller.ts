7964fd5270c903538ae4a5e0ecac53d9e8c1754a
Nurdin <mrsunnysummer@gmail.com>
2024-07-04 14:27:35 +0700

    Create user api route

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,5 +1,15 @@
 /* eslint-disable prettier/prettier */
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
-@Controller('/api/users') // membuat path controller nya
-export class UserController {}
+@Controller('/api/users') // * membuat path controller nya
+export class UserController {
+    @Post() // * karena path nya tidak kita tuliskan maka akan mengikuti path controller
+    registerUser(): string {
+        return 'POST';
+    }
+
+    @Get('/current') // * pathnya akan menjadi /api/users/current
+    getUserCurrent(): string {
+        return 'GET';
+    }
+}
