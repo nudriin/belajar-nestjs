492c12a7306b0fd98be4f18b447cd9b38d84a887
Nurdin <mrsunnysummer@gmail.com>
2024-07-04 20:49:01 +0700

    Request parameter

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,5 +1,5 @@
-/* eslint-disable prettier/prettier */
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('/api/users') // * membuat path controller nya
 export class UserController {
@@ -11,5 +11,27 @@ export class UserController {
     @Get('/current') // * pathnya akan menjadi /api/users/current
     getUserCurrent(): string {
         return 'GET';
+    }
+
+    // mengambil request parameter menggunakan object Request dari express
+    @Get('/:nurdinId') // path yang dapat menerima request param
+    getUserById(@Req() request: Request): string {
+        return `GET ${request.params.nurdinId}`;
+    }
+
+    // mengambil query parameter menggunakan decorator @Query
+    // diakses menggunakan /api/users?name=nurdin
+    @Get()
+    getUserByUsername(@Query('name') name: string): string {
+        return `Hello ${name || 'Guest'}`;
+    }
+
+    // Mengambil request param menggunakan @Param
+    @Get('/:nurdinId/:email')
+    getUserByEmail(
+        @Param('email') email: string,
+        @Param('nurdinId') id: string,
+    ): string {
+        return `ID: ${id} <br>Email: ${email}`;
     }
 }
