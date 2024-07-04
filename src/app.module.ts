cd3de1d2941d194a19f3da2fa57750385efa63e2
Nurdin <mrsunnysummer@gmail.com>
2024-07-04 14:09:32 +0700

    Crete user module and user controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/user/user.controller.ts
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+import { Controller } from '@nestjs/common';
+
+@Controller('/api/users') // membuat path controller nya
+export class UserController {}
