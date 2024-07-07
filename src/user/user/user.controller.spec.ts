701c71559e95269048a6f73be0dc6069dc25e213
Nurdin <mrsunnysummer@gmail.com>
2024-07-07 20:58:47 +0700

    Unit test and install mocks

--- src/user/user/user.controller.spec.ts
+++ src/user/user/user.controller.spec.ts
@@ -15,4 +15,15 @@ describe('UserController', () => {
     it('should be defined', () => {
         expect(controller).toBeDefined();
     });
+
+    it('should can sample response', async () => {
+        // cara menggunakan unit testnya cukup panggil controller dan method yang akan di test
+        const response = await controller.asyncMethod('Nurdin', 20);
+        const data = JSON.parse(response);
+        expect(data).toEqual({
+            name: 'Nurdin',
+            age: 20,
+        });
+        console.log(data);
+    });
 });
